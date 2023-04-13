5807a41eda869f2e2cd82a042397c7f4c510ed9a
Tashe <tstefacek92@gmail.com>
2023-04-13 11:18:09 -0500

    first commit

---
+++ Week1_Challenge.js
@@ -0,0 +1,10 @@
+/* 
+Write a function that takes the base and 
+height of a triangle and return its area.
+*/
+
+function triArea(base, height) {
+    return (base * height) / 2;
+}
+
+console.log(triArea(10, 20));
