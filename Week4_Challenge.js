71837339e968a77bc15d79e95e10b004ee1daecd
Tashe <tstefacek92@gmail.com>
2023-05-02 18:49:39 -0500

    Refactored Week 4 Challenge

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -7,11 +7,6 @@ Words may also contain numbers, for example "Hello world123 567"
 function LongestWord(sen) { 
     let rmvPunc = sen.replace(/[^A-Za-z0-9\s]/g,"");
     let senSplit = rmvPunc.split(" ");
-    let senArr = [];
-      for (let i = 0; i < senSplit.length; i++){
-        senArr.push(senSplit[i]);
-        
-      }
-     let longest = senArr.reduce((a, b) => b.length > a.length ? b : a, '');
-    return longest
+    let longestWord = senSplit.reduce((a, b) => b.length > a.length ? b : a, '');
+    return longestWord
     }
